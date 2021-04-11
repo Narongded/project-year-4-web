fff1fbba6ae42786e65f6a17fade7c5d601b0fa9
naive555 <non.naive@gmail.com>
2021-04-12 01:56:17 +0700

    เปลี่ยน prop จาก sownerpdf เป็น sid

--- src/question.js
+++ src/question.js
@@ -295,7 +295,7 @@ class Question extends React.Component {
                                                 this.props.history.push({
                                                     pathname: `/student-lecture/${value.ques_alluser_uid}/${value.teacherpdfid}`,
                                                     state: { pdfpath: value.studentpdf_sid, userid: value.ques_alluser_uid, 
-                                                        page: value.page, pdfname: this.props.location.state.pdfname, pdfid: value.sownerpdf }
+                                                        page: value.page, pdfname: this.props.location.state.pdfname, pdfid: value.sid }
                                                 })
                                             }
                                             }>
